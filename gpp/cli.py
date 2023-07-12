72da4fa5d4821ee692a35877f63d464979baf0b5
Gisle Aas <gisle.aas@uib.no>
2023-07-12 10:28:11 +0200

    Save the conversations

--- gpp/cli.py
+++ gpp/cli.py
@@ -6,9 +6,11 @@ import click
 import openai
 import json
 from pathlib import Path
+from datetime import datetime
 
 basedir = Path.home() / ".gpp"
 basedir.mkdir(exist_ok=True)
+(basedir / "chats").mkdir(exist_ok=True)
 
 openai.api_key = os.getenv("OPENAI_API_KEY") or (basedir / "openai-key.txt").read_text()[:-1]
 
@@ -17,24 +19,26 @@ def print_json(d):
 
 @click.command()
 @click.argument('question', nargs=-1)
+@click.option('--new/--continue', '-n/-c', default=True)
 @click.option('--model', default='gpt-3.5-turbo', show_default=True)
 @click.option('--stream/--no-stream', default=True)
 @click.option('--json/--no-json', 'output_json')
-def main(question, model, stream, output_json):
-  question = ' '.join(question)
+def main(question, new, model, stream, output_json):
+  messages = [
+    {
+      "role": "system",
+      "content": "Du er en ekspert som hjelper til med å forklare hvordan ting henger sammen. "
+                 "Fortrinnsvis ønsker du å svare kort og presist på norsk."
+    },
+    {
+      "role": "user",
+      "content": ' '.join(question),
+    }
+  ]
 
   response = openai.ChatCompletion.create(
     model=model,
-    messages=[
-      {
-        "role": "system",
-        "content": "Du er en ekspert som hjelper til med å forklare hvordan ting henger sammen. Fortrinnsvis ønsker du å svare kort og presist på norsk."
-      },
-      {
-        "role": "user",
-        "content": question,
-      }
-    ],
+    messages=messages,
     temperature=1,
     max_tokens=1024,
     top_p=1,
@@ -43,12 +47,15 @@ def main(question, model, stream, output_json):
     stream=stream,
   )
 
+  answer = []
+
   if stream:
     for chunk in response:
       if output_json:
         print_json(chunk)
       else:
         if d := chunk['choices'][0]['delta']:
+          answer.append(d['content'])
           print(d['content'], end='', flush=True)
     if not output_json:
       print()  # final newline
@@ -56,8 +63,19 @@ def main(question, model, stream, output_json):
     if output_json:
       print_json(response)
     else:
-      print(response['choices'][0]['message']['content'])
+      answer.append(response['choices'][0]['message']['content'])
+      print(answer[-1])
 
+  messages.append(
+    {
+      "role": "assistant",
+      "content": ''.join(answer)
+    }
+  )
+
+  # Save the chat
+  chatfile = basedir / "chats" / ("chat-" + datetime.now().strftime('%Y%m%dT%H%M%S') + '.json')
+  chatfile.write_text(json.dumps(messages, ensure_ascii=False, indent=2))
 
 if __name__ == '__main__':
     main()
